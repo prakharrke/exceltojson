e642dccfee3e6610673a2f9f68f03cbb9b35b9c8
Prakhar <prakhar.rke24@gmail.com>
2018-12-10 00:02:38 +0530

    completed

--- src/main/java/org/exceltojson/App.java
+++ src/main/java/org/exceltojson/App.java
@@ -1,5 +1,7 @@
 package org.exceltojson;
 
+import org.json.JSONObject;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,18 @@ public class App
     public static void main( String[] args ) throws Exception
     {
     	JSONConverter conv = new JSONConverter();
-    	conv.convertToJSONRowWise("C:\\Users\\Prakhar\\Documents\\Book1.xlsx","Sheet1");
+    	JSONObject resp = conv.convertToJSONRowWise("C:\\Users\\Prakhar\\Documents\\Book1.xlsx","Sheet1");
+    	JSONObject temp = new JSONObject ();
+    	temp = resp.getJSONObject("row2");
+    	temp.put("Header1","Updated1");
+    	temp.put("Header2","Updated2");
+    	temp.put("Header3","Updated3");
+    	temp.put("Header4","Updated4");
+    	temp.put("Header5","Updated5");
+    	temp.put("Header6","Updated6");
+    	temp.put("edited",true);
+    	resp.put("row2",temp);
+    	conv.updateExcel("C:\\Users\\Prakhar\\Documents\\Book1.xlsx","Sheet1",resp );
+    	//conv.convertToJSONColumnWise("C:\\Users\\Prakhar\\Documents\\Book1.xlsx", "Sheet1");
     }
 }
--- src/main/java/org/exceltojson/JSONConverter.java
+++ src/main/java/org/exceltojson/JSONConverter.java
@@ -2,9 +2,10 @@ package org.exceltojson;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,6 +14,7 @@ import com.aspose.cells.Cell;
 import com.aspose.cells.Cells;
 import com.aspose.cells.Workbook;
 import com.aspose.cells.Worksheet;
+import com.google.gson.Gson;
 
 public class JSONConverter {
 	private int lastRowCount=0;
@@ -53,9 +55,9 @@ public class JSONConverter {
 			
 			System.out.println(excelJson);
 			
-			
-			workbook.save(path);
 			excel.close();
+			workbook.save(path);
+			
 			
 		}else {
 			
@@ -64,7 +66,7 @@ public class JSONConverter {
 		}
 	}
 	
-	public void convertToJSONRowWise(String path, String sheetName) throws Exception {
+	public JSONObject convertToJSONRowWise(String path, String sheetName) throws Exception {
 		excelJson = new JSONObject();
 		columns = new ArrayList<String>();
 		File excelFile = new File(path);
@@ -85,15 +87,84 @@ public class JSONConverter {
 					Cell cell = cells.get(i,columns.indexOf(columnName));
 					row.put(columnName, cell.getValue());
 				}
-				
+				row.put("edited",false);
+				row.put("rowNum",i);
 				excelJson.put("row" + i, row);
 				
 			}
 			
 			System.out.println(excelJson);
+			excel.close();
+			workbook.save(path);
+		
+			return excelJson;
 		}
 		
-		
+		return new JSONObject();
+	}
+	
+	public void updateExcel(String path,String sheetName,JSONObject updatedJSON) throws Exception {
+		excelJson = new JSONObject();
+		File excelFile = new File(path);
+		if(excelFile.exists()) {
+			FileInputStream excel = new FileInputStream(excelFile);
+			Workbook workbook = new Workbook(excel);
+			Worksheet sheet = workbook.getWorksheets().get(sheetName);
+			Cells cells = sheet.getCells();
+			columnCount = cells.getMaxDataColumn();
+			excelJson = updatedJSON;
+			Object[] keys = excelJson.keySet().toArray();
+			try {
+			for(Object rowKey : keys) {
+				
+				if(excelJson.getJSONObject(rowKey.toString()).getBoolean("edited")) {
+					JSONObject rowObject = new JSONObject();
+					rowObject = excelJson.getJSONObject(rowKey.toString());
+					int rowNum = rowObject.getInt("rowNum");
+					Object[] headers = rowObject.keySet().toArray();
+					Set<String>headersSet = rowObject.keySet();
+					int colIndex = 0;
+					for(int i=0;i<=(columnCount+2);i++) {
+						if(headers[i].toString().equalsIgnoreCase("edited") ||  headers[i].toString().equalsIgnoreCase("rowNum")) {
+							continue;
+						}else {
+							// CALCULATE THE COLUMN INDEX WITH HEADER SAME AS headers[i]
+						for(int j=0;j<=columnCount;j++) {
+							Cell c = cells.get(0,j);
+							if(c.getValue().equals(headers[i].toString())) {
+								colIndex = j;
+								break;
+							}
+						}
+						Cell cell = cells.get(rowNum,colIndex);
+						cell.putValue(rowObject.get(headers[i].toString()));
+						colIndex++;
+						}
+					}
+					
+					
+					
+					// UPDATE THE EXCEL
+				}else {
+					continue;
+				}
+			}
+			excel.close();
+			workbook.save(path);
+			}catch(Exception e) {
+				e.printStackTrace();
+				excel.close();
+				workbook.save(path);
+			}
+		}
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
 }
